48cb71ebd4682a7846ad06be9a7b292491512269
Evgeniy Iakovenko <iakovenkoeg@gmail.com>
2022-12-31 13:19:12 +0700

    fix poiner event

--- src/components/Mimic/ObjectSelector/index.tsx
+++ src/components/Mimic/ObjectSelector/index.tsx
@@ -126,8 +126,8 @@ function ObjectSelector(props: Props) {
       style={{
         width: "100%",
         height: "100%",
-        pointerEvents: "none",
-        userSelect: "none", //FIXME ???
+        // pointerEvents: "none",
+        //userSelect: "none", //FIXME ???
       }}
       onClick={handleClick}
       onPointerDown={handlePointerDown}
