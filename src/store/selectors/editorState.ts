ec3180de9ccb6ccbf3ffe704788c3e8ed45087da
Evgeniy Iakovenko <iakovenkoeg@gmail.com>
2023-01-08 20:07:25 +0700

    refactoring

--- src/store/selectors/editorState.ts
+++ src/store/selectors/editorState.ts
@@ -1,11 +1,16 @@
 export const selectViewPosition = (state: any) =>
   state.editorState.viewPosition;
+
 export const selectSelectionDisabled = (state: any) =>
   state.editorState.selectionDisabled;
+
 export const selectSelectorRect = (state: any) =>
   state.editorState.selectSelectorRect;
+
 export const selectNewElement = (state: any) => state.editorState.newElement;
+
 export const selectLastTakenId = (state: any) => state.editorState.lastTakenId;
+
 export const selectSelectedElements = (state: any) =>
   state.editorState.selected;
 
@@ -16,7 +21,3 @@ export const selectEditorMode = (state: any) => state.editorState.mode;
 
 export const selectEditorOperations = (state: any) =>
   state.editorState.operations;
-
-//************************************************ */
-// export const selectIsMimicTouch = (state: any) =>
-//   state.editorState.isMimicTouch;
